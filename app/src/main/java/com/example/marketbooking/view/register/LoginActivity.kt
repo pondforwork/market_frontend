8c650f1a1efc7d4abea83f6081be4755be5c40c7
pondforwork <150602705+pondforwork@users.noreply.github.com>
2025-03-24 14:30:47 +0700

    redesign login page

--- app/src/main/java/com/example/marketbooking/view/register/LoginActivity.kt
+++ app/src/main/java/com/example/marketbooking/view/register/LoginActivity.kt
@@ -5,23 +5,25 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Visibility
+//import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import com.example.marketbooking.R
 import com.example.marketbooking.api.ApiService
 import com.example.marketbooking.api.RetrofitClient
-import com.example.marketbooking.model.User
 import com.example.marketbooking.utils.UserPreferences
 import com.example.marketbooking.view.RegularBookingActivity
 import kotlinx.coroutines.launch
-import retrofit2.http.Body
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var userPreferences: UserPreferences
@@ -30,7 +32,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         userPreferences = UserPreferences(this)
         setContent {
-            LoginScreen()
+            MaterialTheme {
+                LoginScreen()
+            }
         }
     }
 
@@ -41,29 +45,24 @@ class LoginActivity : AppCompatActivity() {
             if (response.isSuccessful) {
                 response.body()?.let { user ->
                     userPreferences.saveUser(user)
-                    // Login สำเร็จ เข้าสู่หน้าจอง
-                    val userResponse =  userPreferences.getUser()
+                    val userResponse = userPreferences.getUser()
 
-                    if(userResponse!=null){
-                        // ถ้าเป็น ประจำ
+                    if(userResponse != null) {
                         if (userResponse.bookingCategoryId == 1) {
                             startActivity(Intent(this, RegularBookingActivity::class.java))
-                        }else{
-                            // ขาจร
+                        } else {
                             startActivity(Intent(this, RegularBookingActivity::class.java))
                         }
+                        finish()
                     }
-
-
                 }
-                } else {
+            } else {
                 Log.e("API_RESPONSE", "Error: ${response.errorBody()?.string()}")
             }
         } catch (e: Exception) {
             Log.e("API_RESPONSE", "Exception: ${e.message}")
         }
     }
-
 
     @Composable
     fun LoginScreen() {
@@ -72,50 +71,93 @@ class LoginActivity : AppCompatActivity() {
         var passwordVisible by remember { mutableStateOf(false) }
         val scope = rememberCoroutineScope()
 
-        Column(
+        val gradientColors = listOf(
+            Color(0xFFFFa725),  // FFA725
+            Color(0xFFC1D8C3),  // C1D8C3
+            Color(0xFFFFF5E4) ,  // FFF5E4
+            Color.Red   // FFF5E4
+
+        )
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .background(
+                    brush = Brush.verticalGradient(colors = gradientColors)
+                )
         ) {
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                modifier = Modifier.fillMaxWidth(),
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-//            trailingIcon = {
-//                val image = if (passwordVisible)
-//                    Icons.Filled.Visibility
-//                else Icons.Filled.VisibilityOff
-//
-//                IconButton(onClick = { passwordVisible = !passwordVisible }) {
-//                    Icon(imageVector = image, contentDescription = null)
-//                }
-//            }
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    scope.launch {
-                        login(email,password)
-                    }
-                },
-                modifier = Modifier.fillMaxWidth()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Text("Login")
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            "เข้าสู่ระบบ",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.padding(bottom = 16.dp)
+                        )
+
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = { email = it },
+                            label = { Text("อีเมล") },
+                            modifier = Modifier.fillMaxWidth(),
+                            shape = MaterialTheme.shapes.medium
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        OutlinedTextField(
+                            value = password,
+                            onValueChange = { password = it },
+                            label = { Text("รหัสผ่าน") },
+                            modifier = Modifier.fillMaxWidth(),
+                            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            shape = MaterialTheme.shapes.medium,
+//                            trailingIcon = {
+//                                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+////                                    Icon(
+////                                        imageVector = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+////                                        contentDescription = if (passwordVisible) "Hide password" else "Show password"
+////                                    )
+//                                }
+//                            }
+                        )
+
+                        Spacer(modifier = Modifier.height(24.dp))
+
+                        Button(
+                            onClick = {
+                                scope.launch {
+                                    login(email, password)
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp),
+                            shape = MaterialTheme.shapes.medium,
+                            colors = ButtonDefaults.buttonColors( Color(0xFFFFA725)) // เปลี่ยนสีพื้นหลัง
+                        ) {
+                            Text("เข้าสู่ระบบ", color = Color.White) // ปรับสีตัวอักษรให้ตัดกับพื้นหลัง
+                        }
+
+                    }
+                }
             }
         }
-
-
     }
-
 }
